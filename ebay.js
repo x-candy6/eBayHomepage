31e312115ee105b1746b19d7626e938610745219
x-candy6 <candy@coolcatcollectibles.org>
2023-06-13 10:36:18 -0700

    added working function to get item details such as price and category

--- ebay.js
+++ ebay.js
@@ -1,39 +1,72 @@
-const $ = require('jquery');
+const jquery = require('jquery');
+const xml2js = require('xml2js')
+const axios = require('axios');
  // Define API credentials and request parameters
- const app_id = "AndrewC-pokemonw-PRD-a1c66d072-1e0cc2f5";
- const dev_id = "70b73b90-5798-4d89-b0fa-d5136ff8b0e5";
- const cert = "PRD-1c66d07233aa-5b0b-42c1-b602-41e9";
- const token = "v^1.1#i^1#r^1#f^0#I^3#p^3#t^Ul4xMF8wOjA2QkNFRkJFNDY1RUY4ODc1MUUwM0FBODQ0ODQ1NkU5XzNfMSNFXjI2MA==";
- const url = "https://api.ebay.com/wsapi";
- const xml = `<GetOrdersRequest xmlns="urn:ebay:apis:eBLBaseComponents">
-   <RequesterCredentials>
-     <eBayAuthToken>${token}</eBayAuthToken>
-   </RequesterCredentials>
-   <CreateTimeFrom>2022-01-01T00:00:00.000Z</CreateTimeFrom>
-   <CreateTimeTo>2022-01-31T23:59:59.000Z</CreateTimeTo>
-   <OrderStatus>Active</OrderStatus>
- </GetOrdersRequest>`;
+const data = require("./config.json")
+const app_id = data.KeySets.AppID;
+const dev_id = data.KeySets.DevID;
+const cert = data.KeySets.CertID;
+const oauth = data.KeySets.OauthToken;
 
- // Send SOAP request using AJAX
-$.ajax({
-   url: url,
-   type: "POST",
-   data: xml,
-   headers: {
-     "X-EBAY-API-APP-ID": app_id,
-     "X-EBAY-API-DEV-NAME": dev_id,
-     "X-EBAY-API-CERT-NAME": cert,
-     "X-EBAY-API-SITEID": 0, // USA
-     "X-EBAY-API-COMPATIBILITY-LEVEL": 1081.0,
-     "Content-Type": "text/xml"
-   },
-   success: function(response) {
-     // Parse XML response and retrieve order information
-     const orders = $(response).find("Order");
-     const numOrders = orders.length;
-     console.log(`You have ${numOrders} orders that need to be shipped.`);
-   },
-   error: function(xhr, status, error) {
-     console.error(`Error: ${error}`);
-   }
- });
+let res;
+let price;
+let category;
+let categoryID;
+
+
+const url = data.Endpoints.ebayAPI;
+const itemID = 364219680836;
+//console.log(url);
+//dom = new DOMParser().parseFromString(xml, "text/xml")
+
+
+
+function getItem(itemID){
+
+  const requestBody = `
+    <?xml version="1.0" encoding="utf-8"?>
+    <GetItemRequest xmlns="urn:ebay:apis:eBLBaseComponents">
+      <RequesterCredentials>
+        <eBayAuthToken>${oauth}</eBayAuthToken>
+      </RequesterCredentials>
+      <ItemID>${itemID}</ItemID>
+      <DetailLevel>ReturnAll</DetailLevel>
+    </GetItemRequest>
+  `;
+  axios.post(url, requestBody, {
+    headers: {
+      'Content-Type': 'text/json',
+      'X-EBAY-API-CALL-NAME': 'GetItem',
+      'X-EBAY-API-SITEID': 0, // 0 is for the US site
+      'X-EBAY-API-COMPATIBILITY-LEVEL': 1083,
+    },
+  })
+  .then(response => {
+    // console.log(response.data);
+     xml2js.parseString(response.data, (err, result) => {
+     if (err) {
+       console.error("hi," ,err);
+     } else {
+        res = JSON.parse(JSON.stringify(result));
+        price = parseFloat(res.GetItemResponse.Item[0]['ListingDetails'][0]['ConvertedStartPrice'][0]['_'][0]) + 0.99; // Okay this part is bad lol, After parsing and stringifying, the price is set to an integer(the first left most before the decimal), need to find a way to preserve the actual value instead of just adding .99
+        categoryID = res.GetItemResponse.Item[0]['PrimaryCategory'][0]['CategoryID'][0];
+        category = res.GetItemResponse.Item[0]['PrimaryCategory'][0]['CategoryName'][0].toString();
+      //  console.log(res);
+     }
+   });
+  })
+  .catch(error => {
+    // Handle errors here
+    console.error('hi,' ,error);
+  })
+   // Wait for the axios promise to resolve before using myVariable
+   return price, categoryID, category
+} 
+getItem(itemID);
+
+setTimeout(() => {
+  // console.log(res.GetItemResponse.Item[0]['PrimaryCategory'][0]['CategoryName'][0].toString());
+  console.log(price, categoryID, category);
+}, 1000);
+
+
